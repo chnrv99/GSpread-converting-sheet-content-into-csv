57488bb1523447346af8f21544cd8c81c305d5ad
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2023-06-17 14:23:08 +0530

    init

---
+++ index.py
@@ -0,0 +1,36 @@
+import gspread
+
+email = 'gspread-account-84@forward-quanta-390017.iam.gserviceaccount.com'
+print('Email is:',email, ' Add the email to the sharing link')
+
+sa = gspread.service_account(filename='credentials.json')
+
+mainSheetName = 'Email Subscribers'
+sh = sa.open(mainSheetName)
+
+
+sheetName = 'Sheet1'
+wks = sh.worksheet(sheetName)
+
+print('Rows: ', wks.row_count)
+print('Column: ', wks.col_count)
+
+# print(wks.acell('A1').value)
+# print(wks.cell(2,2).value)
+
+# print(wks.get('A1:B2'))
+# dict
+print(wks.get_all_records())
+# list
+# print(wks.get_all_values())
+
+import csv
+
+my_dict = wks.get_all_records()
+headings=['Name','Email']
+
+with open('csvfile.csv','w') as f:
+    # w = csv.DictWriter(f, my_dict.keys())
+    w = csv.DictWriter(f, fieldnames=headings)
+    w.writeheader()
+    w.writerows(my_dict)
